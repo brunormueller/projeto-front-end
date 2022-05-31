8edf41fb65b7ba6b455cebb9fd5e1aaa553c689c
VANDERSON MESKA <vanderson.meska@alunos.sc.senac.br>
2022-05-30 21:31:38 -0300

    tela de tag

--- src/pages/Tag.jsx
+++ src/pages/Tag.jsx
@@ -2,6 +2,7 @@ import { createRef } from "react"
 import { LayoutComponents } from "../components/LayoutComponents"
 import axios from "axios";
 import MyInput from '../components/MyInput'
+import '../components/LayoutComponents/tag.css';
 
 export const Tag = () => {
     const inputTagRef = createRef();
@@ -11,39 +12,55 @@ export const Tag = () => {
     };
 
     const handleOnButtonSaveClick = async () => {
-        
-        console.log("handleOnButtonSaveClick");
-        
         try {
             await axios.post(`/api/Tag`, {
                 Nome: getInputTagRef().getValue()
             });
-
             alert('Cadastrado com Sucesso!')
-            
         } catch {
             alert('Cadastrado falhou!');
         }
     };
 
+    const handleOnButtonKillClick = async () => {
+        try {
+            await axios.post(`/api/Tag`, {
+                Nome: getInputTagRef().getValue()
+            });
+            alert('Excluido com Sucesso!')
+        } catch {
+            alert('Exclusão falhou!');
+        }
+    };
     return (
         <LayoutComponents>
             <div className="login-form">
-                <span className="login-form-title">Cadastrar Tag</span>
-                <span className="login-form-title">
+                <span className="login-form-title">Tag</span>
+                <span className="login-form-title"></span>
 
-                </span>
+                {/* 
                 <div className="wrap-input">
                     <MyInput
                         ref={inputTagRef}
                     />
                      <span className="focus-input" data-placeholder="Tag"></span>
+                </div> */}
+
+                <div className="container-login-form-btn-consutar"
+                    onClick={handleOnButtonKillClick}>
+                    <button className="login-form-btn">Consutar</button>
                 </div>
 
-                < div className="container-login-form-btn"
+                {/* < div className="container-login-form-btn-salvar"
                     onClick={handleOnButtonSaveClick}>
                     <button className="login-form-btn">Salvar</button>
                 </div>
+
+                <div className="container-login-form-btn-excluir"
+                    onClick={handleOnButtonKillClick}>
+                    <button className="login-form-btn">Excluir</button>
+                </div> */}
+
             </div>
         </LayoutComponents>
     )
